1d6908fb53b064740024f83136634cab64dabb74
超哥 <chaoge@chaogedeMBP.lan>
2026-05-04 21:38:42 +0800

    chore: 版本号升至 v1.3.0，日期更新为 2026-05-04

--- server/version.py
+++ server/version.py
@@ -1,9 +1,9 @@
 """
 版本号管理。
 """
-version = "1.1.0"
-__version_info__ = (1, 1, 0)
-__release_date__ = "2026-05-02"
+version = "1.3.0"
+__version_info__ = (1, 3, 0)
+__release_date__ = "2026-05-04"
 author = "cnchaoge"
 license = "MIT"
 
